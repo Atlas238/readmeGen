7098e054f2e684dc6053f99685eb5ee0fc53fbdc
Atlas <jack@bentsen.us>
2021-07-14 15:17:04 -0700

    better license choices and badge links

--- Develop/index.js
+++ Develop/index.js
@@ -18,9 +18,10 @@ const questions = [
     },
 
     questionThree = {
-        type: ``,
-        name: ``,
-        message: ``
+        type: `list`,
+        name: `license`,
+        message: `Which license would you like to use?`,
+        choices: [`GNU AGPLv3`, `GNU GPLv3`, `GNU LGPLv3`, `Mozilla Public License 2.0`, `Apache License 2.0`, `MIT License`, `Boost Software License 1.0`, `The Unlicense`]
     }
 
 ];
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,26 +1,30 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+  let badge;
+
   switch (license) {
-    case `mit` : 
 
-    case `mozilla` :
+    case `gnuagpl` : return badge = `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`;
 
-    case `apache` :
+    case `gnugpl` : return badge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
 
-    case `boost` :
+    case `gnulgpl` : return badge = `[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)`;
 
-    case `unlicense` :
+    case `mozilla` : return badge = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
 
-    case `gnuap` :
+    case `apache` : return badge = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
 
-    case `gnugp` :
+    case `mit` : return badge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
 
-    case `gnulp` :
+    case `boost` : return badge = `[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`;
+
+    case `unlicense` : return badge = `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`;
 
   }
   
 }
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
